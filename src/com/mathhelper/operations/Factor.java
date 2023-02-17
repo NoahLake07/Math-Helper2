edcd802ef01a5ea730663beb90d301f1e542cd16
Noah Lake <noahlake07@gmail.com>
2023-02-17 16:40:33 -0700

    Added functions for simplifying radicals and prime factoring a number

--- src/com/mathhelper/operations/Factor.java
+++ src/com/mathhelper/operations/Factor.java
@@ -6,29 +6,18 @@ import java.util.ArrayList;
 
 public class Factor {
 
-    public static DualNum findGCF(int num){
-        return null;
-    }
+    public static ArrayList<Integer> findPrimeFactors(int number) {
+        ArrayList<Integer> factors = new ArrayList<Integer>();
 
-
-    public static ArrayList<Integer> factorize(int num){
-        DualNum split = null;
-
-        // get all possible factors
-        for (int i = num; i > 0; i--) {
-            if(num%i==0){
-                split = new DualNum(i,num/i);
+        for (int i = 2; i <= number; i++) {
+            if (number % i == 0) {
+                factors.add(i);
+                factors.addAll(findPrimeFactors(number / i));
+                break;
             }
         }
 
-        ArrayList<Integer> factors = new ArrayList<>();
-
-        if(split.canSimplify()){
-            factors = DataMerge.mergeList(factorize(split.a), factorize(split.b));
-            return factors;
-        } else {
-            return factors;
-        }
+        return factors;
     }
 
     public static ArrayList<DualNum> basicFactorize(int num){
@@ -38,31 +27,6 @@ public class Factor {
         for (int i = num; i > 0; i--) {
             if(num%i==0){
                 factors.add(new DualNum(i,num/i));
-            }
-        }
-
-        // remove duplicates
-        for (int i = 0; i < factors.size(); i++) {
-            for (int j = 0; j < factors.size(); j++) {
-                if(factors.get(i).isOppositeOf(factors.get(j))){
-                    factors.remove(j);
-                }
-            }
-        }
-
-        return factors;
-    }
-
-    public static ArrayList<DualNum> basicFactorize(int num, boolean allowOnes){
-        ArrayList<DualNum> factors = new ArrayList<>();
-
-        // get all possible factors
-        for (int i = num; i > 0; i--) {
-            if(num%i==0){
-                DualNum factor = new DualNum(i,num/i);
-                if(allowOnes && factor.contains(1)) {
-                    factors.add(factor);
-                }
             }
         }
 
--- src/com/mathhelper/operations/RadicalSimplify.java
+++ src/com/mathhelper/operations/RadicalSimplify.java
@@ -2,10 +2,63 @@ package com.mathhelper.operations;
 
 import com.mathhelper.data.Radical;
 
+import java.util.ArrayList;
+
+import static com.mathhelper.operations.Factor.findPrimeFactors;
+
 public class RadicalSimplify {
 
-    public static void simplify(Radical radical){
+    public static String simplifyRadicals(String expression) {
+        String simplified = "";
+        ArrayList<String> terms = new ArrayList<String>();
 
+        // Split the input expression into individual terms
+        int i = 0;
+        while (i < expression.length()) {
+            if (expression.charAt(i) == '+' || expression.charAt(i) == '-') {
+                // Found a plus or minus sign, add the current term to the list
+                terms.add(expression.substring(0, i));
+                expression = expression.substring(i);
+                i = 0;
+            } else {
+                i++;
+            }
+        }
+        terms.add(expression); // Add the last term to the list
+
+        // Simplify each term in the list
+        for (String term : terms) {
+            String[] factors = term.split("\\*|/");
+            ArrayList<String> simplifiedFactors = new ArrayList<String>();
+            for (String factor : factors) {
+                if (factor.contains("sqrt")) {
+                    // Found a square root factor, simplify it using the findPrimeFactors method
+                    String inside = factor.substring(factor.indexOf("(") + 1, factor.indexOf(")"));
+                    int number = Integer.parseInt(inside);
+                    ArrayList<Integer> factorsList = findPrimeFactors(number);
+
+                    // Reconstruct the factor as a simplified string
+                    int outside = 1;
+                    for (int factorNum : factorsList) {
+                        if (factorNum == 2) {
+                            outside *= 2;
+                        } else {
+                            simplifiedFactors.add("sqrt(" + factorNum + ")");
+                        }
+                    }
+                    if (outside > 1) {
+                        simplifiedFactors.add(String.valueOf(outside));
+                    }
+                } else {
+                    // Not a square root factor, add it as is to the simplifiedFactors list
+                    simplifiedFactors.add(factor);
+                }
+            }
+            // Combine the simplified factors back into a single term string
+            String simplifiedTerm = String.join("*", simplifiedFactors);
+            simplified += simplifiedTerm;
+        }
+
+        return simplified;
     }
-
 }
